10dfd4591229c64b7c4aaff9e03b62dcaad6cbdc
Chris Arcand <chris@chrisarcand.com>
2019-07-07 19:57:04 -0500

    random_pet

--- random/main.tf
+++ random/main.tf
@@ -1,10 +1,7 @@
-resource "random_pet" "dog" {
-  keepers = {
-    uuid = "${uuid()}" # Force a new name each time
-  }
+resource "random_pet" "animal" {
   length = 3
 }
 
 output "random" {
-  value = "${random_pet.dog.id}"
+  value = "${random_pet.animal.id}"
 }
