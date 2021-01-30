07e71e2d0e9f8d43ed149d77fcaac5a95a76b951
Chris Arcand <chris@chrisarcand.com>
2021-01-29 22:47:00 -0600

    Update syntax

--- random/main.tf
+++ random/main.tf
@@ -1,12 +1,12 @@
 resource "random_pet" "animal" {
   keepers = {
-    uuid = "${uuid()}" # Force a new name each time
+    uuid = uuid() # Force a new name each time
   }
   length = 3
 }
 
 output "random" {
-  value = "${random_pet.animal.id}"
+  value = random_pet.animal.id
 }
 
 # That's it! That's the joke.
