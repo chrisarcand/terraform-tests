f85f813d7912500c36ae6ff8995f652983545fc6
Chris Arcand <chris@chrisarcand.com>
2021-05-10 20:41:13 -0500

    Exciting new things

--- random/main.tf
+++ random/main.tf
@@ -1,17 +1,15 @@
-resource "random_pet" "animal1" {
+resource "random_pet" "always_new" {
   keepers = {
     uuid = uuid() # Force a new name each time
   }
   length = 3
 }
 
-resource "random_pet" "animal2" {
-  keepers = {
-    uuid = uuid() # Force a new name each time
-  }
+resource "random_pet" "animal_trio" {
+  count = 3
   length = 3
 }
 
-output "random" {
-  value = random_pet.animal1.id
+output "animal_names" {
+  value = concat([random_pet.always_new.id], random_pet.animal_trio[*].id)
 }
