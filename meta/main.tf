a0bc950458d6b7a3935c5bb55bda5c72d8dad1ba
Chris Arcand <chris@chrisarcand.com>
2021-06-03 23:05:54 -0500

    Move to a vars file

--- meta/main.tf
+++ meta/main.tf
@@ -2,38 +2,6 @@ provider "tfe" {
   token = var.provider_token
 }
 
-variable "organization_name" {
-  type = string
-}
-
-variable "provider_token" {
-  type = string
-}
-
-variable "oauth_token_id" {
-  type = string
-}
-
-variable "workspaces" {
-  type = map(object({
-    auto_apply = optional(bool)
-    terraform_version = optional(string)
-    execution_mode    = optional(string)
-    allow_destroy_plan = optional(bool)
-    working_directory = optional(string)
-    file_triggers_enabled = optional(bool)
-    trigger_prefixes = optional(list(string))
-    vcs_repo          = optional(object({
-      identifier = string
-      oauth_token_id = optional(string)
-      branch = optional(string)
-      submodule_ingress = optional(bool)
-    }))
-  }))
-
-  description = "This map of objects allows for simplified creation of a workspace with any resources that are always desired to be included with it."
-}
-
 resource "tfe_workspace" "workspaces" {
   for_each = var.workspaces
 
