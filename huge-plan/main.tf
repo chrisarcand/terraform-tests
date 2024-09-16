2b2542559ed0338131975c0e48f6069391729b82
Chris Arcand <chris@chrisarcand.com>
2024-09-16 14:50:29 -0500

    Big plan?

---
+++ huge-plan/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "null" {}
+
+
+variable "large_data" {
+  default = <<EOT
+    iVBORw0KGgoAAAANSUhEUgAAAoAAAAGwCAYAAADlnkSmAAAIJ0lEQVR4Xu3dQW5UMQwF0ODqf9N3g0Q8AQ0RjTph
+    9Z/vHb8Zcf9ZzDBfJdNvExEAAAxGAfFxAIJMIUAAAIgBAABiAABgwAAAAaAAEAAIAAAGQFQACACAAAAMAAAGAAABm
+    AQAAIAABgAAAQkAAAIgAAADIACAgAASAAAAAABkAAADIACAgAAAIAQACABAAAQAIAIAgAAAIAIAgAAQACACAgAAAA
+    MAAQACAAACACAgAAAAQAIAAQAACAAACACAgAAAAIAAgAAAIAACAgAAAAIAAgAAAQACAgAAAAIAQAAAAIAAAIAAgAAA
+    MAAACAgAAAAIAAgAAAQAACAgAAAIAAgAAAQAIAAgAAAIAAgAAAIAAgAAAIAAAAACAgAA
+    EOT
+}
+
+
+resource "null_resource" "large_data_example" {
+  count = 1000000
+
+  triggers = {
+    large_data = var.large_data
+    instance   = count.index
+  }
+}
+
+output "example_output" {
+  value = null_resource.large_data_example[*].id
+}
